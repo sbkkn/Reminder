a84e3c7686a0d4e05931cdd824448075795708b8
Sergey <bakaikin@yandex.ru>
2015-09-13 21:03:17 +0400

    first commit

---
+++ app/src/main/java/com/bakaikin/sergey/reminder/MainActivity.java
@@ -0,0 +1,73 @@
+package com.bakaikin.sergey.reminder;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+
+    PreferenceHelper preferenceHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PreferenceHelper.getInstance().init(getApplicationContext());
+        preferenceHelper = PreferenceHelper.getInstance();
+
+        fragmentManager = getFragmentManager();
+
+
+        runSplash();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        splashItem.setChecked((preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE)));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_splash) {
+            item.setChecked(!item.isChecked());
+            preferenceHelper.putBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE,item.isChecked());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void runSplash() {
+
+        if (!preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE)) {
+            SplashFragment splashFragment = new SplashFragment();
+
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, splashFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+
+}
+
